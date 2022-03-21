4d1828bfe9c01e3c879ac5aab7c625244ee4512d
jc2476 <97968604+jc2476@users.noreply.github.com>
2022-03-20 22:13:57 -0400

    Fixed menu test

--- tests/simple_pages_test.py
+++ tests/simple_pages_test.py
@@ -5,7 +5,7 @@ def test_request_main_menu_links(client):
     """This makes the index page"""
     response = client.get("/")
     assert response.status_code == 200
-    assert b'<a class="nav-link" href="/">Home</a>' in response.data
+    assert b'<a class="nav-link" aria-current="page" href="/">Home</a>' in response.data
     assert b'<a class="nav-link" href="/git">Git</a>' in response.data
     assert b'<a class="nav-link" href="/docker">Docker</a>' in response.data
     assert b'<a class="nav-link" href="/pf">Python/Flask</a>' in response.data
